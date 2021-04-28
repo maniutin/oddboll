fd88698444d2eb997dc83ab1f5714e21c402fed4
Nick <maniutin@gmail.com>
2021-04-28 14:30:01 -0400

    install react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
