a766260942637db309a3c51922bb6ebbef4e60d5
Nick <maniutin@gmail.com>
2021-05-13 13:50:41 -0400

    add compact article elements to ArticleCompact component

--- client/src/components/ArticleCompact.js
+++ client/src/components/ArticleCompact.js
@@ -1,14 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import "./ArticleCompact.css";
 
 const ArticleCompact = () => {
   return (
-    <div className="album-cover">
-      <img
-        src="images/kotra-and-zavoloka-silence-2020-cover.jpeg"
-        alt="album cover"
-      />
+    <div className="article-compact">
+      <Router>
+        <div className="album-cover">
+          <Link to="/kotra-and-zavoloka-silence-2020">
+            <img
+              src="images/kotra-and-zavoloka-silence-2020-cover.jpeg"
+              alt="album cover"
+            />
+          </Link>
+        </div>
+        <div className="article-category">Review</div>
+        <div className="publication-date">May 5, 2021</div>
+        <div className="article-excerpt">
+          <p>This is the best article ever, wow!</p>
+        </div>
+        <div className="article-read-more">
+          <Link to="/kotra-and-zavoloka-silence-2020">Read More</Link>
+        </div>
+      </Router>
     </div>
   );
 };
