cebdec7ee6e9e9680ac13146aeca8ab0ff349797
Nick <maniutin@gmail.com>
2021-05-20 19:57:02 -0400

    minor lint changes

--- client/src/components/ArticleCompact.js
+++ client/src/components/ArticleCompact.js
@@ -16,21 +16,25 @@ const ArticleCompact = () => {
             />
           </Link>
         </div>
+
         <div className="article-compact-text">
           <div className="article-compact-metadata">
             <div className="article-category">Review</div>
             <div className="publication-date">April 30, 2021</div>
           </div>
+
           <div className="article-title">
             <Link to="/luca-yupanqui-sounds-of-the-unborn-2021">
               Luca Yupanqui, “Sounds Of The Unborn” (2021)
             </Link>
           </div>
+
           <div className="article-excerpt">
             <p className="excerpt">
               Probably the first album by a yet unborn artist
             </p>
           </div>
+
           <div className="article-read-more">
             <Link to="/luca-yupanqui-sounds-of-the-unborn-2021">Read More</Link>
           </div>
