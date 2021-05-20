7bd503505a4cef97244ba44d37893d44e076b251
Nick <maniutin@gmail.com>
2021-05-20 19:13:28 -0400

    create & setup Article component

---
+++ client/src/components/Article.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import "./Article.css";
+
+const ArticleCompact = () => {
+  return <div>Article</div>;
+};
+
+export default ArticleCompact;
