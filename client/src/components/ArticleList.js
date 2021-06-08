c3ea417d1752e01e6953550da46169749d9e6b9a
Nick <maniutin@gmail.com>
2021-06-08 11:47:59 -0400

    remove test import useArticleData from ArticleList

--- client/src/components/ArticleList.js
+++ client/src/components/ArticleList.js
@@ -1,10 +1,8 @@
 import React from "react";
-import useArticleData from "../hooks/useArticleData";
 import ArticleCompact from "./ArticleCompact";
 import "./ArticleList.css";
 
 const ArticleList = () => {
-  const { state } = useArticleData();
   return <ArticleCompact />;
 };
 
