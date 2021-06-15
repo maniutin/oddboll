eca6a0690e1e1041d3a78d8e8ad456050984bdd0
Nick <maniutin@gmail.com>
2021-06-15 12:52:30 -0400

    update seeds with proper excerpt

--- server/db/seeds/01_seeds.sql
+++ server/db/seeds/01_seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO articles (title, tags, category, content, date_published, excerpt)
 VALUES ('Luca Yupanqui, “Sounds Of The Unborn” (2021)', 
-ARRAY['experimental', 'electronic', 'ambient', '2021', '20s', 'USA'], 'Review', 'REVIEW GOES HERE', '2021-04-30', 'Excerpt here');
+ARRAY['experimental', 'electronic', 'ambient', '2021', '20s', 'USA'], 'Review', 'REVIEW GOES HERE', '2021-04-30', 'Probably the first album by a yet unborn artist');
 
 
 
