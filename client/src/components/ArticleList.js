3a2d6377d39a60b76c8b6574dbf6229ce01c90f3
Nick <maniutin@gmail.com>
2021-06-15 13:15:35 -0400

    move useArticleData to App

--- client/src/components/ArticleList.js
+++ client/src/components/ArticleList.js
@@ -1,18 +1,14 @@
 import React from "react";
 import ArticleCompact from "./ArticleCompact";
 import "./ArticleList.css";
-import useArticleData from "../hooks/useArticleData";
 
-const ArticleList = () => {
-  const { articleInfo } = useArticleData();
-  console.log("ARTICLE INFO: ", articleInfo.articleData[0]);
-
+const ArticleList = (props) => {
   return (
     <ArticleCompact
-      category={articleInfo.articleData[0].category}
-      datePublished={articleInfo.articleData[0].date_published}
-      title={articleInfo.articleData[0].title}
-      excerpt={articleInfo.articleData[0].excerpt}
+      category={props.category}
+      datePublished={props.date_published}
+      title={props.title}
+      excerpt={props.excerpt}
     />
   );
 };
